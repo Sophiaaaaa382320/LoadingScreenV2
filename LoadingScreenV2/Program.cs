6856658251bcddabe00b9f5afe3223dd512090c4
Sophiaaaaa382320 <philosophia.alfarabi123@gmail.com>
2026-09-25 13:31:25 +0700

    trying use "while"

--- LoadingScreenV2/Program.cs
+++ LoadingScreenV2/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int totalsteps = 100;
+int a=1;
+while (a <= totalsteps)
+{
+    Console.WriteLine($"Loading.. Step {a} of {totalsteps}");
+    if (a % 3 == 0)
+    {
+        Console.WriteLine("Checkpoint reached!");
+    }
+    a++;
+}
+Console.WriteLine("\nDungeon is ready. Enter now!");
